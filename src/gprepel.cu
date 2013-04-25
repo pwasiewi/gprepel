1aa36882bc82e6ee83ce049cc0bea65033a818d3
Peter Wasiewicz <pwasiewi@elka.pw.edu.pl>
2013-04-25 17:14:40 +0200

    setw

--- src/gprepel.cu
+++ src/gprepel.cu
@@ -167,7 +167,7 @@ void printvec(size_t m, size_t n, thrust::device_vector<T>& d_data)
     thrust::host_vector<T> h_data = d_data;
 
     for(size_t i = 0; i < m*n; i++)
-        std::cout << setw(4) << h_data[i] << " ";
+        std::cout << std::setw(4) << h_data[i] << " ";
     std::cout << "\n";
 }
 
